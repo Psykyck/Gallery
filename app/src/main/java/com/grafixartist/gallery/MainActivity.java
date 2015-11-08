1ff8e5413dbaf601b7edd014486084825c0fa4f6
Clement Lu <lu.705@osu.edu>
2015-11-08 17:53:12 -0500

    Read from media store

--- app/src/main/java/com/grafixartist/gallery/MainActivity.java
+++ app/src/main/java/com/grafixartist/gallery/MainActivity.java
@@ -1,7 +1,11 @@
 package com.grafixartist.gallery;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,29 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<ImageModel> data = new ArrayList<>();
 
-    public static String IMGS[] = {
-           "https://images.unsplash.com/photo-1444090542259-0af8fa96557e?q=80&fm=jpg&w=1080&fit=max&s=4b703b77b42e067f949d14581f35019b",
-            "https://images.unsplash.com/photo-1439546743462-802cabef8e97?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1441155472722-d17942a2b76a?q=80&fm=jpg&w=1080&fit=max&s=80cb5dbcf01265bb81c5e8380e4f5cc1",
-            "https://images.unsplash.com/photo-1437651025703-2858c944e3eb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1431538510849-b719825bf08b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1434873740857-1bc5653afda8?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1439396087961-98bc12c21176?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1433616174899-f847df236857?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438480478735-3234e63615bb?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300",
-            "https://images.unsplash.com/photo-1438027316524-6078d503224b?dpr=2&fit=crop&fm=jpg&h=725&q=50&w=1300"
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < IMGS.length; i++) {
+        ArrayList<String> IMGS = getImagesPath(MainActivity.this);
+
+        for (int i = 0; i < IMGS.size(); i++) {
 
             ImageModel imageModel = new ImageModel();
             imageModel.setName("Image " + i);
-            imageModel.setUrl(IMGS[i]);
+            imageModel.setUrl(IMGS.get(i));
             data.add(imageModel);
 
         }
@@ -69,6 +62,26 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }));
 
+    }
+
+    public static ArrayList<String> getImagesPath(Activity activity) {
+        Uri uri;
+        ArrayList<String> listOfAllImages = new ArrayList<>();
+        Cursor cursor;
+        int column_index_data;
+        String PathOfImage;
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA};
+
+        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        while (cursor.moveToNext()) {
+            PathOfImage = cursor.getString(column_index_data);
+            listOfAllImages.add(PathOfImage);
+        }
+        return listOfAllImages;
     }
 
 }

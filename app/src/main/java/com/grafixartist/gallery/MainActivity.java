66c04c4c28ceecea91d1660aea5de54e7d95de40
confuzzed03 <kim.4256@osu.edu>
2015-11-08 19:40:33 -0500

    Added getting title and size of picture

--- app/src/main/java/com/grafixartist/gallery/MainActivity.java
+++ app/src/main/java/com/grafixartist/gallery/MainActivity.java
@@ -109,19 +109,35 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> listOfAllImages = new ArrayList<>();
         Cursor cursor;
         int column_index_data;
+        int title_index_data;
+        int size_index_data;
         String PathOfImage;
+        String TitleOfImage;
+        String SizeOfImage;
         uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
         String[] projection = {MediaStore.MediaColumns.DATA,
                                MediaStore.MediaColumns.DISPLAY_NAME,
-                               MediaStore.MediaColumns.DATE_ADDED
+                               MediaStore.MediaColumns.DATE_ADDED,
+                                // ADDED THIS LINE TO GET DATA FROM SIZE COLUMN
+                                MediaStore.MediaColumns.SIZE
                               };
 
         cursor = activity.getContentResolver().query(uri, projection, null, null, MediaStore.Images.Media.DATE_TAKEN + " DESC");
 
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+
+        // THIS IS WHERE INDEX OF TITLE AND SIZE COLUMNS ARE FOUND
+        title_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME);
+        size_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME);
+
         while (cursor.moveToNext()) {
             PathOfImage = cursor.getString(column_index_data);
+
+            // THIS IS WHERE TITLE AND SIZE ARE RETRIEVED
+            TitleOfImage = cursor.getString(title_index_data);
+            SizeOfImage = cursor.getString(size_index_data);
+
             listOfAllImages.add(PathOfImage);
         }
         //close(cursor);

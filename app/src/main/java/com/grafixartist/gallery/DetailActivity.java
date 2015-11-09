c8e1739b93fa85b16e1613bcc406bc11cd66a3c9
darkbunnie <sun.762@buckeyemail.osu.edu>
2015-11-09 02:02:14 -0500

    Created menus for main page

--- app/src/main/java/com/grafixartist/gallery/DetailActivity.java
+++ app/src/main/java/com/grafixartist/gallery/DetailActivity.java
@@ -8,8 +8,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -91,26 +93,14 @@ public class DetailActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInf) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
+        super.onCreateContextMenu(menu, v, menuInf);
+        //menu.setHeaderTitle("Sort By");
+        getMenuInflater().inflate(R.menu.context_menu, menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
     /**
@@ -232,6 +222,7 @@ public class DetailActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
             final ImageView imageView = (ImageView) rootView.findViewById(R.id.detail_image);
+            registerForContextMenu(imageView);
 
             Glide.with(getActivity()).load(url).thumbnail(0.1f).into(imageView);
 

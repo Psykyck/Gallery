fb7a43ed34129d118549fd0f07f5e9b17dbd0d00
darkbunnie <sun.762@buckeyemail.osu.edu>
2015-11-09 20:35:44 -0500

    Advanced settings page added

---
+++ app/src/main/java/com/grafixartist/gallery/ChangeEmail.java
@@ -0,0 +1,13 @@
+package com.grafixartist.gallery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ChangeEmail extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.email_confirm);
+    }
+}
---
+++ app/src/main/java/com/grafixartist/gallery/ChangePassword.java
@@ -0,0 +1,13 @@
+package com.grafixartist.gallery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ChangePassword extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.password_confim);
+    }
+}
